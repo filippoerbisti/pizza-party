1d16ad97a340ead693179107ec89e27d9fc2b4f7
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-13 09:50:56 +0200

    move folder components

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+// import Footer from './components/Footer'
+// import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/* <Navbar /> */}
+
       <Head>
         <title>Pizza Party</title>
         <meta name="description" content="Best Pizza Shop in Town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      ok
+
+      {/* <Footer /> */}
     </div>
   )
 }
