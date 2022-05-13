ebceb985f5d31c0f6d6c058453506f948b828b6b
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-13 14:58:30 +0200

    upload

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,9 +1,56 @@
+import styles from "../styles/Footer.module.css"
+import Image from "next/image"
+
 const Footer = () => {
     return (
-        <div>
-            <footer>
-                footer
-            </footer>
+        <div className={styles.container}>
+
+            <div className={styles.item}>
+                <Image src="/img/bg.png" layout="fill" alt="footer" />
+            </div>
+
+            <div className={styles.item}>
+                <div className={styles.card}>
+                    <h2 className={styles.motto}>
+                        THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA.
+                    </h2>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+                    <p className={styles.text}>
+                        1654 R. Don Road #304.
+                        <br /> NewYork, 85022
+                        <br /> (602) 867-1010
+                    </p>
+                    <p className={styles.text}>
+                        1654 R. Don Road #304.
+                        <br /> NewYork, 85022
+                        <br /> (602) 867-1010
+                    </p>
+                    <p className={styles.text}>
+                        1654 R. Don Road #304.
+                        <br /> NewYork, 85022
+                        <br /> (602) 867-1010
+                    </p>
+                    <p className={styles.text}>
+                        1654 R. Don Road #304.
+                        <br /> NewYork, 85022
+                        <br /> (602) 867-1010
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>WORKING HOURS</h1>
+                    <p className={styles.text}>
+                        MONDAY UNTIL FRIDAY
+                        <br /> 9:00 - 22:00
+                    </p>
+                    <p className={styles.text}>
+                        SATURDAY - SUNDAY
+                        <br /> 12:00 - 24:00
+                    </p>
+                </div>
+            </div>
+            
         </div>
     )
 }
