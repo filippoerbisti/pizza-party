bd97c728fd5a6cb64115607ebebb53f40a18ef9b
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-16 11:52:53 +0200

    add routing click on img/title

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,15 +1,16 @@
 import styles from "../styles/PizzaCard.module.css"
 import Image from "next/image"
+import Link from "next/link"
 
 const PizzaCard = ({pizza}) => {
   return (
     <div className={styles.container}>
-        <Image src={pizza.img} alt="pizza" width="500" height="500" />
-        <h1 className={styles.title}>{pizza.title}</h1>
+        <Link href={`/product/${pizza._id}`} passHref>
+          <Image src={pizza.img} alt="pizza" width="500" height="500" />
+          <h1 className={styles.title}>{pizza.title}</h1>
+        </Link>
         <span className={styles.price}>${pizza.prices[0]}</span>
-        <p className={styles.desc}>
-            {pizza.desc}
-        </p>
+        <p className={styles.desc}>{pizza.desc}</p>
     </div>
   )
 }
