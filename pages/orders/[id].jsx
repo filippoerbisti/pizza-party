ccc0ed327f30d2198692f9e2e58c3fccf1a9f82d
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-16 09:35:05 +0200

    fix err: 'Hydration failed because the initial UI does not match what was rendered on the server'

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -17,28 +17,30 @@ const Order = () => {
         <div className={styles.left}>
             <div className={styles.row}>
                 <table className={styles.table}>
-                    <tr className={styles.trTitle}>
-                        <th>Order ID</th>
-                        <th>Customer</th>
-                        <th>Address</th>
-                        <th>Total</th>
-                    </tr>
-                    <tr className={styles.tr}>
-                        <td>
-                            <span className={styles.id}>123456789</span>
-                        </td>
-                        <td>
-                            <span className={styles.name}>
-                                Filippo Erbisti
-                            </span>
-                        </td>
-                        <td>
-                            <span className={styles.address}>Elton st. 212-33 LA</span>
-                        </td>
-                        <td>
-                            <span className={styles.total}>$79.80</span>
-                        </td>
-                    </tr>
+                    <tbody>
+                        <tr className={styles.trTitle}>
+                            <th>Order ID</th>
+                            <th>Customer</th>
+                            <th>Address</th>
+                            <th>Total</th>
+                        </tr>
+                        <tr className={styles.tr}>
+                            <td>
+                                <span className={styles.id}>123456789</span>
+                            </td>
+                            <td>
+                                <span className={styles.name}>
+                                    Filippo Erbisti
+                                </span>
+                            </td>
+                            <td>
+                                <span className={styles.address}>Elton st. 212-33 LA</span>
+                            </td>
+                            <td>
+                                <span className={styles.total}>$79.80</span>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
 
