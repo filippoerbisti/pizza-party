e2bb31cb39724f182c27cfa7d66b7e0275648f98
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-16 16:32:21 +0200

    api/order/:id

--- pages/api/orders/[id].js
+++ pages/api/orders/[id].js
@@ -13,7 +13,12 @@ const handler =  async (req, res) => {
     await dbConnect();
 
     if (method === "GET") {
-        
+        try {
+            const order = await Order.findById(id);
+            res.status(200).json(order);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 
     if (method === "PUT") {
