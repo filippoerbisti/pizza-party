06a4b8149c4561d73252dba60cdb9ac2eb18e6aa
Filippo Erbisti <filippo.erbisti@gmail.com>
2022-05-16 11:49:52 +0200

    dynamic data w/ axios

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,14 +1,14 @@
 import styles from "../styles/PizzaCard.module.css"
 import Image from "next/image"
 
-const PizzaCard = () => {
+const PizzaCard = ({pizza}) => {
   return (
     <div className={styles.container}>
-        <Image src="/img/pizza.png" alt="pizza" width="500" height="500" />
-        <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-        <span className={styles.price}>$19.90</span>
+        <Image src={pizza.img} alt="pizza" width="500" height="500" />
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>${pizza.prices[0]}</span>
         <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {pizza.desc}
         </p>
     </div>
   )
